2719b4a6d5f5a0a39d0697bda7a3468b345c4acb
lkcz04185 <lkcz04185@gmail.com>
2017-02-09 21:06:24 +0000

    task creation

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -25,4 +25,15 @@ RSpec.describe TasksController, type: :controller do
       expect(task.done).to eq(true)
     end
   end
+   describe "task#create" do
+    it "should allow a new task to be created" do
+      post :create, task: {title: "Make bed!"}
+      expect(response).to have_http_status(:success)
+      response_value = ActiveSupport::JSON.decode(@response.body)
+      expect(response_value['title']).to eq("Make bed!")
+      expect(Task.last.title).to eq("Make bed!")
+    end
+  end
+
+
 end
